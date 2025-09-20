47003f2fb4ec8953582ba9efc89e8024988cf070
DAKSH4567 <dakshjain2122iva5@mpspnjpr.com>
2025-09-20 15:56:58 +0530

    initial commit

---
+++ py2/um.py
@@ -0,0 +1,8 @@
+a = 3
+print(type(a))
+b = 2.5
+print(type(b))
+c = "coding"
+print(type(c))
+d = True
+print(type(d))
---
+++ py2/umm.py
@@ -0,0 +1,16 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("name :", name, type(name))
+print("age :", age, type(age))
+print("is_student :", is_student, type(is_student))
+print("weight :", weight, type(weight))
+
+# after type casting
+age = str(age)
+print("age :", age, type(age))
+
+weight = int(weight)
+print("weight :", weight, type(weight))
