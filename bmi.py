c63fa5ae21dc1ac0eb60028dbe5be517711ad743
DAKSH4567 <dakshjain2122iva5@mpspnjpr.com>
2025-10-03 20:25:57 +0530

    initial commit

--- bmi.py
+++ bmi.py
@@ -8,7 +8,7 @@ BMI = weight / (height / 100) ** 2
 print("Your BMI is", BMI)
 
 if BMI <= 18.4:
-    print("You are underweight.")
+     print("You are underweight.")
 elif BMI <= 24.9:
     print("You are healthy.")
 elif BMI <= 29.9:
