76157310cb0bbc78619bb003bfdbe7534e180587
DAKSH4567 <dakshjain2122iva5@mpspnjpr.com>
2025-10-03 21:15:46 +0530

    initial commit

---
+++ looooped.py
@@ -0,0 +1,18 @@
+# Count total digits in a number using while loop
+
+num = int(input("Enter a number: "))
+count = 0
+
+# Handle the case if number is 0
+if num == 0:
+    count = 1
+else:
+    # Make sure to work with positive numbers
+    if num < 0:
+        num = -num
+
+    while num > 0:
+        num = num // 10
+        count += 1
+
+print("Total digits:", count)
